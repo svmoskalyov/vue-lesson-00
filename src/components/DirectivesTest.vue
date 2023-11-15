34a044b5436be0642c52bcbb0286a737da53ce24
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-11-15 12:38:05 +0200

    Create DirectivesTest.vue

---
+++ src/components/DirectivesTest.vue
@@ -0,0 +1,44 @@
+<template>
+  <div v-if="isVisible">
+    <h1>Directives</h1>
+    <h1 v-show="false">Directives show</h1>
+
+    <ul>
+      <li v-for="fruit in fruits" :key="fruit">{{ fruit }}</li>
+    </ul>
+
+    <ul>
+      <template v-for="fruit in fruits">
+        <li v-if="fruit === 'banana'" :key="fruit">{{ fruit }}</li>
+      </template>
+    </ul>
+
+    <ul>
+      <li v-for="(car, key) in cars" :key="key">
+        {{ key + " - " + car.color }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DirectivesTest",
+  data() {
+    return {
+      isVisible: true,
+      fruits: ["kiwi", "banana", "apple"],
+      cars: {
+        audi: {
+          color: "red",
+        },
+        bmw: {
+          color: "black",
+        },
+      },
+    };
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>

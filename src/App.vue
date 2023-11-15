4086519af44ccbf2429a495b1b185513c405257e
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-11-15 23:37:36 +0200

    added ApartmentsItem

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,76 @@
 <template>
-  <h1>{{ title }}</h1>
-  <Button @click="increments" outlined>Click me</Button>
-  <DirectivesTest />
-  <StarRating :rating="3.5" />
+  <div :id="$style.app">
+    <ApartmentsItem
+      :descr="apartment.descr"
+      :price="apartment.price"
+      :rating="apartment.rating"
+      :imgSrc="`https://c4.wallpaperflare.com/wallpaper/427/193/66/evening-nordland-europe-starry-night-wallpaper-preview.jpg`"
+    />
+  </div>
 
-  <!-- <div id="app"></div> -->
+  <!-- <h1>{{ title }}</h1> -->
+  <!-- <Button @click="increments" outlined>Click me</Button> -->
+  <!-- <DirectivesTest /> -->
+  <!-- <StarRating :rating="3.5" /> -->
+
   <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
 <script>
+import ApartmentsItem from "./components/apartment/ApartmentsItem.vue";
 // import HelloWorld from './components/HelloWorld.vue'
-import Button from "./components/Button.vue";
-import DirectivesTest from "./components/DirectivesTest.vue";
-import StarRating from "./components/StarRating.vue";
+// import Button from "./components/Button.vue";
+// import DirectivesTest from "./components/DirectivesTest.vue";
+// import StarRating from "./components/StarRating.vue";
 
 export default {
   name: "App",
   components: {
+    ApartmentsItem,
     // HelloWorld
-    Button,
-    DirectivesTest,
-    StarRating,
+    // Button,
+    // DirectivesTest,
+    // StarRating,
   },
   data() {
     return {
-      amountOfClicks: 0,
+      apartment: {
+        id: "5cwtgf472gvd28csajhvcs8",
+        title: "Apartment title",
+        descr: "bla-bla-bla, bla-bla-bla, ..., bla-bla-bla, bla-bla-bla.",
+        price: 1045,
+        rating: 4.7,
+        location: {
+          city: "Odessa",
+        },
+        owner: {
+          name: "Kosel",
+          phone: "123-123-1234",
+          email: "asd@gmail.com",
+        },
+      },
     };
   },
-  computed: {
-    title() {
-      return `Amount of clicks ${this.amountOfClicks}`;
-    },
-  },
-  methods: {
-    increments() {
-      this.amountOfClicks += 1;
-    },
-  },
+  // data() {
+  //   return {
+  //     amountOfClicks: 0,
+  //   };
+  // },
+  // computed: {
+  //   title() {
+  //     return `Amount of clicks ${this.amountOfClicks}`;
+  //   },
+  // },
+  // methods: {
+  //   increments() {
+  //     this.amountOfClicks += 1;
+  //   },
+  // },
 };
 </script>
 
-<style>
+<style module>
 #app {
   font-family: Montserrat, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;

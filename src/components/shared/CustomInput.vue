1a7e58ef29c6eb7fa694bc3ae99025d679a4a84c
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-11-16 17:35:51 +0200

    Create CustomInput.vue

---
+++ src/components/shared/CustomInput.vue
@@ -0,0 +1,30 @@
+<template>
+  <input v-on="listeners" class="custom-input" />
+</template>
+
+<script>
+export default {
+  name: "CustomInput",
+  computed: {
+    listeners() {
+      return {
+        // eslint-disable-next-line vue/no-deprecated-dollar-listeners-api
+        ...this.$listeners,
+        input: (event) => this.$emit("input", event.target.value),
+      };
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/scss/variables.scss";
+.custom-input {
+  min-height: 40px;
+  border: 2px solid $main-color;
+  font-size: 18px;
+  outline: none;
+  line-height: inherit;
+  padding: 8px 15px;
+}
+</style>

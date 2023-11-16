d309f5c1f8abcd847b09413e0f3a388098608468
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-11-16 13:06:47 +0200

    Create Container.vue

---
+++ src/components/shared/Container.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="container">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Container",
+};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 1024px;
+  margin: 0 auto;
+  padding: 0 20px;
+}
+</style>

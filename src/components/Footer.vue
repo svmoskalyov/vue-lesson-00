d37c4e60056134e9534a2863dfc9753fb6ecae77
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-11-22 13:39:40 +0200

    Create Footer.vue

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="footer"></footer>
+</template>
+
+<script>
+export default {
+  name: "Footer",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../assets/scss/variables.scss";
+.footer {
+  background: $secondary-color;
+  height: 30px;
+}
+</style>

3225cb97effb915683aec3d7375178f9927c7772
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-11-22 13:39:35 +0200

    Create Logo.vue

---
+++ src/components/Logo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="logo">
+    <img src="../assets/logo.png" alt="logo img" class="logo__img" />
+    <span class="logo__text">Logo</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Logo",
+};
+</script>
+
+<style lang="scss" scoped>
+.logo {
+  display: flex;
+  text-align: center;
+  gap: 4px;
+}
+.logo__img {
+  width: 18px;
+  height: 18px;
+}
+
+.logo__text {
+  color: #fff;
+}
+</style>

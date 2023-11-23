227a0e52f71033402fc4efd8822517b5aa8360fd
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-11-23 18:07:52 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import './assets/scss/index.scss'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./assets/scss/index.scss";
+import router from "./router";
 
-createApp(App).mount('#app')
+// createApp(App).mount('#app')
+
+const app = createApp(App);
+
+app.use(router);
+app.mount("#app");

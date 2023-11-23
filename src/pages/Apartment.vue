cdf3b79554bbe92f43ddbf186abef79b4b5c3ddc
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-11-23 18:08:50 +0200

    Create Apartment.vue

---
+++ src/pages/Apartment.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <Container>
+      <h1>Apartment Page</h1>
+    </Container>
+  </div>
+</template>
+
+<script>
+import Container from "../components/shared/Container.vue";
+
+export default {
+  name: "Apartment",
+  components: {
+    Container,
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>

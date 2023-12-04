23033170e8d29a4d6439a8f36473dc942e749d7f
Serhii Moskalov <sv.moskalyov@gmail.com>
2023-12-04 13:28:52 +0200

    added persisted store

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,16 @@
 import { createStore } from "vuex";
+import createPersistedState from "vuex-plugin-persistedstate";
 import authModule from "./modules/auth";
 
 const store = createStore({
   modules: {
     auth: authModule,
   },
+  plugins: [
+    createPersistedState({
+      paths: ["auth.token"],
+    }),
+  ],
 });
 
 export default store;
